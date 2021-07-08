d98b81e01a0df8db47b94a8ef8829d07d724bb3d
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-07-07 23:59:11 -0400

    commit tree 215ca943795a2c5adc392de37e5a1c91af8470a5

---
+++ lib/insert.sql
@@ -0,0 +1,80 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (
+        1,
+        "Mr.Chocolate",
+        3,
+        "M",
+        "Brown",
+        "Angry",
+        "true"
+    );
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (
+        2,
+        "Rowdy",
+        3,
+        "M",
+        "Brown",
+        "Angry",
+        "true"
+    );
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (
+        3,
+        "Tabitha",
+        3,
+        "M",
+        "Brown",
+        "Angry",
+        "true"
+    );
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (
+        4,
+        "Sergeant Browm",
+        3,
+        "M",
+        "Brown",
+        "Angry",
+        "true"
+    );
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (
+        5,
+        "Melissa",
+        3,
+        "M",
+        "Brown",
+        "Angry",
+        "true"
+    );
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (
+        6,
+        "Grinch",
+        3,
+        "M",
+        "Brown",
+        "Angry",
+        "true"
+    );
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (
+        7,
+        "Wendy",
+        3,
+        "M",
+        "Brown",
+        "Angry",
+        "true"
+    );
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (
+        8,
+        NULL,
+        NULL,
+        "M",
+        "Brown",
+        "Angry",
+        "true"
+    );
